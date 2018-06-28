065b845f665b97df6a051260ee50c3d98363e51c
Bryce Hayden <bryce-hayden@pluralsight.com>
2018-06-28 17:00:00 -0600

    Added note about when to use state, props, or redux

--- web/src/ui/pages/secured/challenges/string-manipulations/index.js
+++ web/src/ui/pages/secured/challenges/string-manipulations/index.js
@@ -1,10 +1,10 @@
 // TODO Challenge 4
 
 // Create a parent container component
-//    Have an input that will take a phrase and pass it's value down to its two children
+//    Have an input that will take a phrase and pass it's value down to its two children (save the input data to redux-saga state tree)
 //    Have two children components (make the folders here)
 //        One that will show the reverse of the string
-//            This component should have a radio input that will change the direction from being normal to reverse
+//            This component should have a radio input that will change the direction from being normal to reverse (only use props & comp state)
 //            It should effect both it's output but also the output for the filtered component
 //        One that will filter out characters from the string
-//            This component will have an additional input that will be used to filter characters
+//            This component will have an additional input that will be used to filter characters (only use props & comp state)
